1f540b4aa8dc93f7d48aee867f75104da54a3a31
Fathima farhana <yfarhanait@gmail.com>
2019-11-11 15:11:04 +0530

    [Santhiya Fathima] Add. Should not park same vehicle again

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,17 +1,24 @@
 package com.parkinglot;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ParkingLot {
+    private static int capacity;
     private int availableSpace;
+    private Set<Parkable> parkedVechile= new HashSet<Parkable>();
 
     public ParkingLot(int availableSpace)
     {
+        capacity=availableSpace;
         this.availableSpace=availableSpace;
     }
 
-    public boolean park() {
-        if (isSpaceAvailable()) {
-            availableSpace--;
-            return true;
+    public boolean park(Parkable vehicle) {
+        if (isSpaceAvailable() && parkedVechile.add(vehicle)) {
+                availableSpace--;
+                return true;
         }
         return false;
     }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -1,23 +1,39 @@
 package com.parkinglot;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
+
+    class ParkableTest implements Parkable {
+        @Override
+        public boolean park() {
+            return false;
+        }
+    }
+
     @Test
-    void shouldBeSuccessfullyParkWhenSpaceAvailable()
-    {
-        ParkingLot parkinglot =  new ParkingLot(1);
-        boolean isParked = parkinglot.park();
+    void shouldBeSuccessfullyParkWhenSpaceAvailable() {
+        ParkingLot parkinglot = new ParkingLot(1);
+        boolean isParked = parkinglot.park(new ParkableTest());
         Assertions.assertTrue(isParked);
     }
+
     @Test
-    void shouldNotParkWhenSpaceNotAvailable()
-    {
-        ParkingLot parkinglot =  new ParkingLot(0);
-        boolean isParked = parkinglot.park();
+    void shouldNotParkWhenSpaceNotAvailable() {
+        ParkingLot parkinglot = new ParkingLot(0);
+        boolean isParked = parkinglot.park(new ParkableTest());
 
         Assertions.assertFalse(isParked);
+    }
+
+    @Test
+    void shouldNotParkWhenTheSameVehicleAlreadyParked() {
+        ParkingLot parkinglot = new ParkingLot(2);
+        Parkable vehicle = new ParkableTest();
+        parkinglot.park(vehicle);
+        Assertions.assertFalse(parkinglot.park(vehicle));
     }
 }
